91902353ae9827e9bb71381614003d0678395eb9
Rody Davis <rody@rive.app>
2020-02-13 19:21:25 -0800

    base file test

--- file_manager/tests/file_test.dart
+++ file_manager/tests/file_test.dart
@@ -1,110 +1,20 @@
-import 'package:flutter/services.dart';
-import 'package:flutter_test/flutter_test.dart';
+import 'package:file/memory.dart';
+import 'package:file/file.dart';
+import 'package:test/test.dart';
+
+var fs = MemoryFileSystem();
 
 void main() {
-  // test('shortcuts work', () {
-  //   var keyBinding = ShortcutKeyBinding(
-  //     [
-  //       Shortcut(
-  //         ShortcutAction.copy,
-  //         [
-  //           ShortcutKey.systemCmd,
-  //           ShortcutKey.c,
-  //         ],
-  //       ),
-  //       Shortcut(
-  //         ShortcutAction.paste,
-  //         [
-  //           ShortcutKey.systemCmd,
-  //           ShortcutKey.v,
-  //         ],
-  //       ),
-  //       Shortcut(
-  //         ShortcutAction.cut,
-  //         [
-  //           ShortcutKey.systemCmd,
-  //           ShortcutKey.x,
-  //         ],
-  //       ),
-  //       Shortcut(
-  //         ShortcutAction.undo,
-  //         [
-  //           ShortcutKey.systemCmd,
-  //           ShortcutKey.z,
-  //         ],
-  //       ),
-  //       Shortcut(
-  //         ShortcutAction.redo,
-  //         [
-  //           ShortcutKey.shift,
-  //           ShortcutKey.systemCmd,
-  //           ShortcutKey.z,
-  //         ],
-  //       ),
-  //     ],
-  //   );
+  setUp(() async {
+    final File outputFile = fs.file('base.txt');
+    await outputFile.writeAsString('Hello world!');
+  });
 
-  //   // Shortcut system automatically determines which command key to use based
-  //   // on platform, so let's similarly pick the appropriate one that maps our
-  //   // abstraction to check for.
-  //   var systemCommandKeyLeft = Platform.instance.isMac
-  //       ? PhysicalKeyboardKey.metaLeft
-  //       : PhysicalKeyboardKey.controlLeft;
-
-  //   var systemCommandKeyRight = Platform.instance.isMac
-  //       ? PhysicalKeyboardKey.metaRight
-  //       : PhysicalKeyboardKey.controlRight;
-  //   // Verify that key chords resolve to expected actions
-  //   expect(
-  //     keyBinding.lookupAction(
-  //       [
-  //         systemCommandKeyLeft,
-  //         PhysicalKeyboardKey.keyC,
-  //       ],
-  //     ),
-  //     [
-  //       ShortcutAction.copy,
-  //     ],
-  //   );
-
-  //   // Out of order works too
-  //   expect(
-  //     keyBinding.lookupAction(
-  //       [
-  //         PhysicalKeyboardKey.keyC,
-  //         systemCommandKeyLeft,
-  //       ],
-  //     ),
-  //     [
-  //       ShortcutAction.copy,
-  //     ],
-  //   );
-
-  //   // Either left or right key...
-  //   expect(
-  //     keyBinding.lookupAction(
-  //       [
-  //         systemCommandKeyRight,
-  //         PhysicalKeyboardKey.keyC,
-  //       ],
-  //     ),
-  //     [
-  //       ShortcutAction.copy,
-  //     ],
-  //   );
-
-  //   // Out of order works too
-  //   expect(
-  //     keyBinding.lookupAction(
-  //       [
-  //         PhysicalKeyboardKey.shiftLeft,
-  //         systemCommandKeyRight,
-  //         PhysicalKeyboardKey.keyZ,
-  //       ],
-  //     ),
-  //     [
-  //       ShortcutAction.redo,
-  //     ],
-  //   );
-  // });
+  test('base file exists', () {
+    final _file = fs.file('base.txt');
+    expect(
+      _file.existsSync(),
+      equals(true),
+    );
+  });
 }
