84c3733a6d0366243939db4948112246166395f3
Rody Davis <rody@rive.app>
2020-02-13 19:40:26 -0800

    updating tests

--- file_manager/tests/file_test.dart
+++ file_manager/tests/file_test.dart
@@ -1,20 +1,29 @@
 import 'package:file/memory.dart';
-import 'package:file/file.dart';
 import 'package:test/test.dart';
 
 var fs = MemoryFileSystem();
 
 void main() {
   setUp(() async {
-    final File outputFile = fs.file('base.txt');
-    await outputFile.writeAsString('Hello world!');
+    await fs.file('baseTest.txt').writeAsString('Hello world!');
+    await fs.file('outputTest.txt').writeAsString('This is a test string');
+    await fs.file('binaryTest.txt').writeAsBytes([0, 2, 3, 7, 3, 2, 4, 32, 3]);
   });
 
   test('base file exists', () {
-    final _file = fs.file('base.txt');
+    final _file = fs.file('baseTest.txt');
     expect(
       _file.existsSync(),
       equals(true),
+    );
+  });
+
+  test('check contents of file', () {
+    final _file = fs.file('outputTest.txt');
+    final _content = _file.readAsStringSync();
+    expect(
+      _content,
+      equals('This is a test string'),
     );
   });
 }
