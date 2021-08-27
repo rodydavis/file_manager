890b72110503b917e2d4ec7320e7f2fd01ed9840
Rody Davis <rody.davis.jr@gmail.com>
2021-08-26 19:32:59 -0700

    formatting

--- lib/src/drop/io.dart
+++ lib/src/drop/io.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 import '../../file_manager.dart';
 
-
 class DropZone extends StatefulWidget {
   final Widget child;
   final ValueChanged<List<FileBase>> onFilesDropped;
